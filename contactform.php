a71e905cfa385a12e73c42df686a6533dcf01004
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-07 09:44:04 +0000

    initial save

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$mailTo = "blank@site.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received a message from ".$name.".\n\n".$message;
+}
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsent")
+
+?>
