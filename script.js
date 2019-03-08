37ef81380e998698a92a914bcfcc6678248c5ae2
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-08 17:42:03 +0000

    test

--- script.js
+++ script.js
@@ -6,30 +6,29 @@ request.onload = function () {
 	var results = JSON.parse(request.responseText);
 	console.log(results);
 	
+	var treehouseStats = `
+		<table>
+			<tr>
+				<th>Points: </th>
+				<td>${results.points.total}</td>
+			</tr>
+			<tr>
+				<th>HTML: </th>
+				<td>${results.points.HTML}</td>
+			</tr>
+			<tr>
+				<th>CSS: </th>
+				<td>${results.points.CSS}</td>
+			</tr>
+			<tr>
+				<th>JavaScript: </th>
+				<td>${results.points.JavaScript}</td>
+			</tr>
+			<tr>
+				<th>Python: </th>
+				<td>${results.points.Python}</td>
+			</tr>
+		</table>`
+		
+	stats.innerHTML = treehouseStats;
 }
-console.log('test');
-var treehouseStats = `
-	<table>
-		<tr>
-			<th>Points: </th>
-			<td>${results.points.total}</td>
-		</tr>
-		<tr>
-			<th>HTML: </th>
-			<td>${results.points.HTML}</td>
-		</tr>
-		<tr>
-			<th>CSS: </th>
-			<td>${results.points.CSS}</td>
-		</tr>
-		<tr>
-			<th>JavaScript: </th>
-			<td>${results.points.JavaScript}</td>
-		</tr>
-		<tr>
-			<th>Python: </th>
-			<td>${results.points.Python}</td>
-		</tr>
-	</table>`
-	
-stats.innerHTML = treehouseStats;
