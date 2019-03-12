a83901ad7f40da08290db13acdd19c5dd4f96986
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-12 21:32:28 +0000

    added js scrolling highlight and html section classes

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
-var statsTH = document.getElementById('statsTH');
-var statsCW = document.getElementById('statsCW');
-console.log('boo');
+$(window).scroll(function() {
+    var position = $(this).scrollTop();
 
-var request = new XMLHttpRequest();
-request.open('GET', 'https://teamtreehouse.com/adamwhite11.json');
-request.onload = function () {
-	let results = JSON.parse(request.responseText);
-	console.log(results);
-	
-	var treehouseStats = `
-		<table>
-			<tr>
-				<th>Points: </th>
-				<td>${results.points.total}</td>
-			</tr>
-			<tr>
-				<th>HTML: </th>
-				<td>${results.points.HTML}</td>
-			</tr>
-			<tr>
-				<th>CSS: </th>
-				<td>${results.points.CSS}</td>
-			</tr>
-			<tr>
-				<th>JavaScript: </th>
-				<td>${results.points.JavaScript}</td>
-			</tr>
-			<tr>
-				<th>Python: </th>
-				<td>${results.points.Python}</td>
-			</tr>
-		</table>`
+    $('.section').each(function() {
+        var target = $(this).offset().top;
+        var id = $(this).attr('id');
 		
-	statsTH.innerHTML = treehouseStats;
-}
+        if (position >= target) {
+			$('#navbarNav > ul > li > a').removeClass('active'); 
+            $(`#navbarNav > ul > li > a[href='#${id}']`).addClass('active');
+        }
+    });
+});
 
-request.send();
-
-
-
-// big issue with codewars api and cors - had to implement alternative solution
-
-
-
-
-
-// on learning place most recent katas by iterating through completed challenged object
