c3825d6397a3de45864e426128a2596a705b86fc
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-15 18:20:33 +0000

    added offset to scrolling function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(window).scroll(function() {
-    var $position = $(this).scrollTop();
+    var $position = $(this).scrollTop() + 100;
 
     $('.section').each(function() {
         var target = $(this).offset().top;
         var id = $(this).attr('id');
 		
-        if ($position - 50>= target) {
+        if ($position>= target) {
 			$('#navbarNav > ul > li > a').removeClass('active'); 
             $(`#navbarNav > ul > li > a[href='#${id}']`).addClass('active');
         }
